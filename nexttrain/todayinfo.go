33fc69750e5d1a61a82528fe6cd559ae9d3bca8f
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-04-13 04:30:27 +0900

    カレンダーAPIが返してくるCSVを読み込むルーチンを追加。

--- nexttrain/todayinfo.go
+++ nexttrain/todayinfo.go
@@ -3,20 +3,77 @@ package nexttrain
 import (
 	"net/http"
 	"io/ioutil"
+	"time"
+	"fmt"
+	"encoding/csv"
+	"strings"
+	"io"
+	"strconv"
+	"unicode/utf8"
 )
 
 
 // CalenderAPIのURL
-// const date_api_url string = "http://calendar-service.net/cal?start_year=2010&start_mon=1&end_year=&end_mon=&year_style=normal&month_style=ja&wday_style=ja&format=json"
-const date_api_url string = "http://calendar-service.net/cal?start_year=2010&start_mon=1&end_year=&end_mon=&year_style=normal&month_style=numeric&wday_style=en&format=csv"
+const date_api_url string = "http://calendar-service.net/cal?start_year=%d&start_mon=%d&end_year=&end_mon=&year_style=normal&month_style=numeric&wday_style=en&format=csv"
 
-func GetCalenderInfoJson() string {
+// 日付情報を入れる構造体
+type DateInfo struct {
+	Date        time.Time
+	DayNoOfWeek int
+	SpecialDay  bool
+}
 
-	resp, _ := http.Get(date_api_url)
+
+func GetCalenderInfoCsv(target time.Time) string {
+
+	// 対象日(というより対象日の月)を指定したURL作成
+	url := MakeApiUrlByDate(target)
+	// HTTP通信にて、CSVを取得
+	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 
 	byteArray, _ := ioutil.ReadAll(resp.Body)
 
-
 	return string(byteArray) // htmlをstringで取得
+}
+
+// 指定した日付のカレンダーAPIのURLを作成し取得
+func MakeApiUrlByDate(target time.Time) string {
+	return fmt.Sprintf(date_api_url, target.Year(), target.Month())
+}
+
+func ConvDateInfosByCsv(csvText string) []DateInfo {
+	// 配列を作成
+	dateInfos := []DateInfo{}
+	// 一行ずつカンマ区切りを処理
+	csvReader := csv.NewReader(strings.NewReader(csvText))
+
+	isFirst := true
+	for {
+		record, err := csvReader.Read() // 1行読み出す
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		if isFirst {
+			isFirst = false
+			continue
+		}
+
+		// 無事読めたようだ。日付作成。
+		year, _ := strconv.Atoi(record[0])
+		mon, _ := strconv.Atoi(record[1])
+		day, _ := strconv.Atoi(record[2])
+		dayNoOfWeek , _ := strconv.Atoi(record[6])
+		date := time.Date(year, time.Month(mon), day, 0, 0, 0, 0, time.Local)
+		// 構造体作成
+		ti := DateInfo{}
+		ti.Date = date
+		ti.DayNoOfWeek = dayNoOfWeek   // 曜日
+		ti.SpecialDay = (utf8.RuneCountInString(record[7]) > 0)
+		// 配列に追加
+		dateInfos = append(dateInfos, ti)
+	}
+	return dateInfos
 }
--- nexttrain/todayinfo_test.go
+++ nexttrain/todayinfo_test.go
@@ -1,15 +1,74 @@
 package nexttrain
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"strings"
+)
+
+
+// テスト用Csvデータ
+// TODO ファイル読み込みにしたい…
+const Testcsv string = `年,月,日,年号,和暦,曜日,曜日番号,祝日名
+2010,01,01,平成,22,Fri,5,元日
+2010,01,02,平成,22,Sat,6,
+2010,01,03,平成,22,Sun,0,
+2010,01,04,平成,22,Mon,1,
+2010,01,05,平成,22,Tue,2,
+2010,01,06,平成,22,Wed,3,
+2010,01,07,平成,22,Thu,4,
+2010,01,08,平成,22,Fri,5,
+2010,01,09,平成,22,Sat,6,
+2010,01,10,平成,22,Sun,0,
+2010,01,11,平成,22,Mon,1,成人の日
+2010,01,12,平成,22,Tue,2,
+2010,01,13,平成,22,Wed,3,
+2010,01,14,平成,22,Thu,4,
+2010,01,15,平成,22,Fri,5,
+2010,01,16,平成,22,Sat,6,
+2010,01,17,平成,22,Sun,0,
+2010,01,18,平成,22,Mon,1,
+2010,01,19,平成,22,Tue,2,
+2010,01,20,平成,22,Wed,3,
+2010,01,21,平成,22,Thu,4,
+2010,01,22,平成,22,Fri,5,
+2010,01,23,平成,22,Sat,6,
+2010,01,24,平成,22,Sun,0,
+2010,01,25,平成,22,Mon,1,
+2010,01,26,平成,22,Tue,2,
+2010,01,27,平成,22,Wed,3,
+2010,01,28,平成,22,Thu,4,
+2010,01,29,平成,22,Fri,5,
+2010,01,30,平成,22,Sat,6,
+2010,01,31,平成,22,Sun,0,
+`
+
+// 日付によってURLを組み立てる。
+func TestMakeApiUrlByDate(t *testing.T) {
+	targetDate := time.Date(2001, 12, 31, 0, 0, 0, 0, time.Local)
+	actual := MakeApiUrlByDate(targetDate)
+	if !strings.Contains(actual, "start_year=2001") || !strings.Contains(actual , "start_mon=12") {
+		t.Log("作成したURL")
+		t.Log(actual)
+		t.Errorf("MakeApiUrlByDate() is faild.")
+	}
+}
 
 // カレンダーAPIから内容が取れるか
 // TODO 外部に接続するテストなんで、外部依存をなくすよう改造したい。
 func TestGetCalenderInfoJson(t *testing.T) {
-	actual := GetCalenderInfoJson()
-	if len(actual) <= 0 || true {
+	actual := GetCalenderInfoCsv(time.Now())
+	if len(actual) <= 0 {
 		t.Log("取得できた文字列")
 		t.Log(actual)
 		t.Errorf("GetCalenderInfoJson() is faild.")
 	}
 }
 
+// カレンダーAPいから取り出したCSVから構造体の配列へ変換する
+func TestConvDateInfosByCsv(t *testing.T) {
+	actual := ConvDateInfosByCsv(Testcsv)
+	if len(actual) != 31 {
+		t.Errorf("ConvDateInfosByCsv() is faild.")
+	}
+}
