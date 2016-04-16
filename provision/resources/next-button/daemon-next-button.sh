de8a155bbb5518507ab2dc531203f9020632a5a3
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-04-16 20:14:59 +0900

    とりあえずは「ボタンを押せば動く」というルーチンを作成。(ただしCPU負荷たかし)

--- provision/resources/next-button/daemon-next-button.sh
+++ provision/resources/next-button/daemon-next-button.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
-# sample usojissou
-watch echo 'test desuyo!' >> /tmp/test.txt
+#
+# next-button のデーモンスクリプト。
+#
+# 無限ループにて「ボタンが押されたかどうか」を判定する。
 
+# 自身のスクリプトのパスを取得する。
+CUR_DIR=$(cd $(dirname $0);pwd)
+LIB_DIR=/usr/local/next-button
+STATUS_FILE=/tmp/next-button.status
+LOG_FILE=/var/log/next-button.log
+BUTTUN_EVENT=${LIB_DIR}/next-button.bsh
+
+# GPIO系定数
+GPIO_NO=2
+INTERVAL_SEC=0.1
+GPIO_DIR=/sys/class/gpio
+GPIO_VALUE=${GPIO_DIR}/gpio${GPIO_NO}/value
+
+# GPIO周りを初期化
+echo ${GPIO_NO} > ${GPIO_DIR}/export
+sleep 1
+echo in > ${GPIO_DIR}/gpio${GPIO_NO}/direction
+
+# 初回ステータスファイルを入れて置く
+echo '0' > ${STATUS_FILE}
+
+while true ; do
+  # インターバルスリープ
+  sleep ${INTERVAL_SEC}
+  # ボタンのステータスを取得
+  last_button_status=`cat ${STATUS_FILE}`
+  now_button_status=`cat ${GPIO_VALUE}`
+  echo ${now_button_status} > ${STATUS_FILE}
+  # 0->1へ変化した時のみ
+  if [[ ${last_button_status} != ${now_button_status} ]] && [[ ${now_button_status} = 1 ]] ; then
+    date >> ${LOG_FILE}
+      echo "異なった場合のルーチンに入ってきている : ${now_button_status}" >>  ${LOG_FILE}
+    # 「実際に行いたい処理」のスクリプトを
+    ${BUTTUN_EVENT}
+  fi
+done
+
+# コレが実行されることはない…うまいことしたい
+echo ${GPIO_NO} > /sys/class/gpio/unexport
+
+exit 0
