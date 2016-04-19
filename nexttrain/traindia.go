f9960b388ce91292a5fb1386d6ce1a905e2a6e1e
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-04-19 22:14:10 +0900

    とりあえず、URL作成関数を実装。テスト未。

--- nexttrain/traindia.go
+++ nexttrain/traindia.go
@@ -2,7 +2,11 @@ package nexttrain
 
 import (
 	"time"
+	"fmt"
 )
+
+const train_dia_url string = "http://www.ekikara.jp/newdata/ekijikoku/2701062/down1_27212011%s.htm"
+
 
 // 鉄道のダイヤ情報の一つを表す構造体
 type TrainTimeInfo struct {
@@ -31,5 +35,12 @@ func ScrapeTrainTimeInfo(url string) []TrainTimeInfo {
 
 // 指定された日付情報から、時刻表のページのURLを組み立てる。
 func MakeTrainTimeUrl(targetDate DateInfo) string {
-	return "仮実装"
+	diaSufix := ""	//
+	// まず、日曜or休日なら
+	if (targetDate.SpecialDay || targetDate.DayNoOfWeek == 0) {
+		diaSufix = "_holi"
+	} else if (targetDate.DayNoOfWeek == 6) {
+		diaSufix = "_sat"
+	} // それ以外はなにもつけなくてよし。
+	return fmt.Sprintf(train_dia_url, diaSufix)
 }
